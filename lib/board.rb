d8f1b814c1f64b6f5b96e82a8e234107f465d69b
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-25 22:01:58 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,29 @@
-class board
 row = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
 
 
-# Position 1: Left
+# Position 1: Top Left
 row[0]
 
-# Position 2: Middle
+# Position 2: Top Middle
 row[1]
 
-# Position 3: Right
+# Position 3: Top Right
 row[2]
+
+# Position 4: Middle Left
+row[3]
+
+# Position 5: Middle Middle
+row[4]
+
+# Position 6: Middle Right
+row[5]
+
+# Position 7: Bottom Left
+row[6]
+
+# Position 8: Bottom Middle
+row[7]
+
+# Position 9: Bottom Right
+row[8]
